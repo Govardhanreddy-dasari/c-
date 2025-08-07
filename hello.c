364ffa5eb2adb08258644b1f704bd97d56c4a9d6
govardhan <govardhanreddydasari07>
2025-08-07 15:11:01 +0530

    sum operation

--- hello.c
+++ hello.c
@@ -4,5 +4,9 @@ int main(){
     printf("\nfsdfdsf");
     printf("\nhello world");
     printf("\ngovardhan reddy");
+    int i =0;
+    int j=12;
+    int k=i+j;
+    printf("sum is",k);
     return 0;
 }
