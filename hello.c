1b27027b3c70e9c9805b85c9543939194e3d9c07
govardhan <govardhanreddydasari07>
2025-08-09 10:48:43 +0530

    hello world

--- hello.c
+++ hello.c
@@ -1,12 +1,8 @@
 #include<stdio.h>
-
 int main(){
-    printf("\nfsdfdsf");
-    printf("\nhello world");
-    printf("\ngovardhan reddy");
-    int i =0;
-    int j=12;
-    int k=i+j;
-    printf("sum is",k);
-    return 0;
+    printf("hello world");
+}
+#include<stdio.h>
+int main(){
+    printf("hii govardhan");
 }
---
+++ operators.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+int main(){
+    int i =10;
+    int j=15;
+    printf(i+j);
+}
